5e19698989d28bff652a5d56c71e196f52a8fb2e
KamilErtap <k.ertap32500@gmail.com>
2025-12-20 17:12:06 +0300

    a

---
+++ frontend/src/components/Admin.jsx
@@ -0,0 +1,15 @@
+function Admin() {
+  return (
+    <div>
+      <InputWithLabel
+        id="email"
+        label="E-Posta:"
+        type="text"
+        isFocused
+      />
+    </div>
+  );
+}
+
+// Bileşeni dışa aktar
+export default Admin;
