e0098536a40078c4a3cbd4506b192d62a20e905c
KamilErtap <k.ertap32500@gmail.com>
2025-12-22 13:32:51 +0300

    a

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,16 +1,41 @@
 import { Link } from "react-router-dom";
 import InputWithLabel from "./InputWithLabel";
 import Header from "./Header";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 function Signup() {
+    const navigate = useNavigate(); 
+
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
+    const handleSignup = async () => {
+        try {
+            const response = await axios.post("/api/signup", {
+                name,
+                email,
+                password,
+            });
+
+            const token = response.data.token;
+            localStorage.setItem("token", token);
+            navigate("/");
+        } catch (error) {
+            
+        }
+        
+    }
+
     return (
         <div>
             <Header />
             <h1>Kayıt Ol</h1>
-            <InputWithLabel label="Kullanıcı Adı" type="text" />
-            <InputWithLabel label="Email" type="email" />
-            <InputWithLabel label="Şifre" type="password" />
-            <button>Kayıt Ol</button>
+            <InputWithLabel label="Kullanıcı Adı" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+            <InputWithLabel label="Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <InputWithLabel label="Şifre" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <button onClick={handleSignup}>Kayıt Ol</button>
             <p>Zaten hesabınız var mı? Giriş yapın.</p> <Link to="/login">Giriş Yap</Link>
         </div>
     );
