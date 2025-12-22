e00fae56747f0dfd694969ac0814dda4e227cf35
KamilErtap <k.ertap32500@gmail.com>
2025-12-22 12:56:30 +0300

    a

--- frontend/src/components/User.jsx
+++ frontend/src/components/User.jsx
@@ -1,6 +1,9 @@
+import Header from "./Header"; // Sayfa başlığı bileşeni
+
 function User() {
   return (
     <div>
+        <Header />
       <div>
         <button>Giriş Yap</button> <button>Kayıt Ol</button>
       </div>
