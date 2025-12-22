4181c5aacf7acc5e8e0aed519cbcb78dba075618
KamilErtap <k.ertap32500@gmail.com>
2025-12-22 13:21:24 +0300

    a

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,16 @@
+import Header from "./Header";
+import InputWithLabel from "./InputWithLabel";
+import { Link } from "react-router-dom";
+
 function Login() {
     return (
         <div>
+            <Header />
             <h1>Giriş Yap</h1>
+            <InputWithLabel label="Email" type="email" />
+            <InputWithLabel label="Şifre" type="password" />
+            <button>Giriş Yap</button>
+            <p>Hesabınız yok mu? Kayıt olun.</p> <Link to="/signup">Kayıt Ol</Link>
         </div>
     );
 }
