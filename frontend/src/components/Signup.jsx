5ba3bc953f940ebf20180e8f5cfb492e86ac7ea0
KamilErtap <k.ertap32500@gmail.com>
2025-12-22 13:46:16 +0300

    a

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,45 +1,68 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 import InputWithLabel from "./InputWithLabel";
 import Header from "./Header";
-import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 function Signup() {
-    const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
-    const [name, setName] = React.useState("");
-    const [email, setEmail] = React.useState("");
-    const [password, setPassword] = React.useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleSignup = async () => {
-        try {
-            const response = await axios.post("/api/signup", {
-                name,
-                email,
-                password,
-            });
-
-            const token = response.data.token;
-            localStorage.setItem("token", token);
-            navigate("/");
-        } catch (error) {
-            console.error("Kayıt işlemi başarısız:", error);
-            alert("Kayıt işlemi başarısız oldu. Lütfen tekrar deneyin.");
+  const handleSignup = async () => {
+    try {
+      const response = await axios.post(
+        "https://mekanbul-azya.vercel.app/api/signup",
+        {
+          name,
+          email,
+          password,
         }
-        
-    }
+      );
 
-    return (
-        <div>
-            <Header />
-            <h1>Kayıt Ol</h1>
-            <InputWithLabel label="Kullanıcı Adı" type="text" value={name} onChange={(e) => setName(e.target.value)} />
-            <InputWithLabel label="Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-            <InputWithLabel label="Şifre" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            <button onClick={handleSignup}>Kayıt Ol</button>
-            <p>Zaten hesabınız var mı? Giriş yapın.</p> <Link to="/login">Giriş Yap</Link>
-        </div>
-    );
+      localStorage.setItem("token", response.data.token);
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+      alert("Kayıt işlemi başarısız oldu");
+    }
+  };
+
+  return (
+    <div>
+      <Header />
+      <h1>Kayıt Ol</h1>
+
+      <InputWithLabel
+        label="Kullanıcı Adı"
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+
+      <InputWithLabel
+        label="Email"
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <InputWithLabel
+        label="Şifre"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleSignup}>Kayıt Ol</button>
+
+      <p>
+        Zaten hesabınız var mı? <Link to="/login">Giriş Yap</Link>
+      </p>
+    </div>
+  );
 }
 
 export default Signup;
